3d8fb83ad23a4a46c3b9597b9147844bb0510008
delunny <delunny@hotmail.com>
2023-11-27 11:48:51 +0800

    1

--- runtime/core/post_processor/post_processor.h
+++ runtime/core/post_processor/post_processor.h
@@ -19,7 +19,7 @@
 #include <memory>
 #include <string>
 #include <utility>
-#include "post_processor/wetext_processor.h"
+#include "processor/wetext_processor.h"
 #include "utils/utils.h"
 
 namespace wenet {
